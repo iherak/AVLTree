a0c3c82583d85948d03920e67c7a2f484585887f
iherak <hex.skk@gmail.com>
2014-02-05 22:49:13 +0100

    Initial commit

---
+++ src/AVL.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NASP_Labos1
+{
+    public class AVL
+    {
+        public Node root;
+        
+        public class Node
+        {
+            public int  value;
+            //public Node Parent;
+            public Node Left;
+            public Node Right;
+            public int  height;
+
+            public Node(int val)
+            {
+                value = val;
+                //Parent = null;
+                Left = null;
+                Right = null;
+                height = 0;
+            }
+        }
+
+        private int max(int v1, int v2)
+        {
+            return ((v1 > v2) ? v1 : v2);
+        }
+        private void setHeight(Node node)
+        {
+            int m,n,d;
+            m = height(node.Left);
+            n = height(node.Right);
+            d = max(m, n);
+            node.height = d+1;
+        }
+
+        public int height(Node nd)
+        {
+            int h;
+            if (nd == null) return -1;
+            else
+            {
+                h = nd.height;
+                return h;
+            }
+        }
+
+        public Node insert(int val, Node node)
+        {
+            if (node == null)
+            {
+                Node newNode = new Node(val);
+                setHeight(newNode);
+                return newNode;
+            }
+            else
+            {
+                if (val < node.value)
+                {
+                    node.Left = insert(val, node.Left);
+                    if ((height(node.Left)-height(node.Right))==2)
+                    {
+                        if (val < node.Left.value) node = RotateLeft(node);
+                        else node = RotateLeftRight(node);
+                    }
+                    setHeight(node);
+                    return node;
+                }
+                else if (val > node.value)
+                {
+                    node.Right = insert(val, node.Right);
+                    if ((height(node.Right) - height(node.Left)) == 2)
+                    {
+                        if (val > node.Right.value) node = RotateRight(node);
+                        else node = RotateRightLeft(node);
+                    }
+                    setHeight(node);
+                    return node;
+                }
+                else throw new System.ArgumentException("Broj je vec u stablu!");
+            }
+        }
+
+        public Node delete(int val, ref Node node)
+        {
+            bool flag = true;
+
+            if (node == null)
+                throw new System.ArgumentException("Element ne postoji!");
+            else if (val < node.value)
+                delete(val, ref node.Left);
+            else if (val > node.value)
+                delete(val, ref node.Right);
+            else
+            {
+                if ((node.Left == null) && (node.Right == null))
+                {
+                    node = null;
+                    flag = true;
+                }
+                else if (node.Left == null)
+                {
+                    node = node.Right;
+                    flag = true;
+                }
+                else if (node.Right == null)
+                {
+                    node = node.Left;
+                    flag = true;
+                }
+                else
+                    node.value = findMin(node.Right);
+            }
+            if (!flag) balance(node);
+            return node;
+        }
+
+        private void balance(Node node)
+        {
+            int fx, dfx;
+
+            fx = height(node.Right) - height(node.Left);
+            if (fx == 2)
+            {
+                dfx = height(node.Right.Right) - height(node.Right.Left);
+                if ((dfx == 0) || (dfx == 1))
+                    node = RotateRight(node);
+                else
+                    node = RotateRightLeft(node);
+            }
+            else if (fx == -2)
+            {
+                dfx = height(node.Left.Right) - height(node.Left.Left);
+                if ((dfx == -1) || (dfx == 0))
+                    node = RotateLeft(node);
+                else
+                    node = RotateLeftRight(node);
+            }
+            setHeight(node);
+        }
+
+        private int findMin(Node node)
+        {
+            int temp;
+
+            if (node.Left == null)
+            {
+                temp = node.value;
+                node = node.Right;
+                return temp;
+            }
+            else
+            {
+                temp = findMin(node.Left);
+                return temp;
+            }
+        }
+
+        private Node RotateLeft(Node node)
+        {
+            Node temp;
+
+            temp = node.Left;
+            node.Left = temp.Right;
+            temp.Right = node;
+            node.height = max(height(node.Left), height(node.Right)) + 1;
+            temp.height = max(node.height, height(temp.Left)) + 1;
+            
+            return temp;
+        }
+
+        private Node RotateRight(Node node)
+        {
+            Node temp;
+
+            temp = node.Right;
+            node.Right = temp.Left;
+            temp.Left = node;
+            node.height = max(height(node.Left), height(node.Right)) + 1;
+            temp.height = max(node.height, height(temp.Right)) + 1;
+            
+            return temp;
+        }
+
+        private Node RotateLeftRight(Node node)
+        {
+            node.Left = RotateRight(node.Left);
+            return RotateLeft(node);
+        }
+
+        private Node RotateRightLeft(Node node)
+        {
+            node.Right = RotateLeft(node.Right);
+            return RotateRight(node);
+        }  
+    }
+}
---
+++ src/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NASP_Labos1
+{
+    public partial class Form1 : Form
+    {
+        public AVL tree;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //tree = new AVL();
+            //tree.root = null;
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            tree = new AVL();
+            OpenFileDialog file = new OpenFileDialog();
+            file.CheckFileExists = true;
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader reader = new StreamReader(file.FileName);
+                string content = reader.ReadToEnd();
+                List<int> numbersList = new List<int>();
+                string[] numbers = content.Split(' ');
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    int num;
+                    if (int.TryParse(numbers[i], out num) && num >= 0)
+                        numbersList.Add(Convert.ToInt32(num));
+                }
+                tree.root = null;
+                foreach (int i in numbersList)
+                {
+                    tree.root = tree.insert(i, tree.root);
+                }
+                reader.Close();
+            }
+            printTree(tree.root, 10);
+        }
+
+        private void printTree(AVL.Node root, int space)
+        {
+            if (root == null)
+            {
+                //textBox1.Text += "\r\n";
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < space; i++)
+                    textBox1.Text += "          ";
+                textBox1.Text += root.value.ToString()+"\r\n";
+            }
+            printTree(root.Right, space+2);
+            printTree(root.Left, space-2);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            if (addTextbox.Text.Length != 0)
+            {
+                tree.root = tree.insert(Convert.ToInt32(addTextbox.Text), tree.root);
+                textBox1.Text = "";
+                printTree(tree.root, 10);
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (delTextBox.Text.Length != 0)
+            {
+                tree.root = tree.delete(Convert.ToInt32(delTextBox.Text), ref tree.root);
+                textBox1.Text = "";
+                printTree(tree.root, 10);
+            }
+        }
+    }
+}
